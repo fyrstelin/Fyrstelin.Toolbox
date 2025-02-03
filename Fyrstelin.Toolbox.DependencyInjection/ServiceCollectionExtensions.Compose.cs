1c59ea9a985fe7d1389d90a95cef1fc5a1fca68b
Andreas Fyrstelin Fuller <afkpost@gmail.com>
2025-02-03 12:09:56 +0100

    ✨ Add decorator pattern

--- Fyrstelin.Toolbox.DependencyInjection/ServiceCollectionExtensions.Compose.cs
+++ Fyrstelin.Toolbox.DependencyInjection/ServiceCollectionExtensions.Compose.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Immutable;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace Fyrstelin.Toolbox.DependencyInjection;
 
@@ -55,45 +53,7 @@ public static partial class ServiceCollectionExtensions
 
         var componentsKey = key == fromKey ? new object() : fromKey;
         
-        services.Add(components.Select(x =>
-        {
-            if (x.ImplementationType is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, x.ImplementationType, x.Lifetime);
-            }
-
-            if (x.KeyedImplementationType is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationType, x.Lifetime);
-            }
-
-            if (x.ImplementationInstance is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, x.ImplementationInstance);
-            }
-
-            if (x.KeyedImplementationInstance is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationInstance);
-            }
-
-            if (x.ImplementationFactory is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, (provider, _) => x.ImplementationFactory(provider), x.Lifetime);
-            }
-
-            if (x.KeyedImplementationFactory is not null)
-            {
-                return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationFactory, x.Lifetime);
-            }
-
-            throw new NotImplementedException($"Cannot create new service descriptor for {x.Lifetime} {x.ServiceType}");
-        }));
-
-        foreach (var descriptor in components)
-        {
-            services.Remove(descriptor);
-        }
+        ChangeKey(services, components, componentsKey);
 
         services.Add(new ServiceDescriptor(typeof(TService), key, (provider, _) =>
         {
@@ -108,6 +68,56 @@ public static partial class ServiceCollectionExtensions
         }, lifetime));
 
         return services;
+    }
+
+    private static void ChangeKey(IServiceCollection services, IEnumerable<ServiceDescriptor> descriptors, object? componentsKey)
+    {
+        var serviceDescriptors = descriptors.ToList();
+        foreach (var descriptor in serviceDescriptors)
+        {
+            services.Remove(descriptor);
+        }
+
+        services.Add(serviceDescriptors.Select(x =>
+        {
+            if (x.IsKeyedService)
+            {
+                if (x.KeyedImplementationType is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationType, x.Lifetime);
+                }
+
+                if (x.KeyedImplementationInstance is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationInstance);
+                }
+
+                if (x.KeyedImplementationFactory is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, x.KeyedImplementationFactory, x.Lifetime);
+                }
+            }
+            else
+            {
+                if (x.ImplementationType is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, x.ImplementationType, x.Lifetime);
+                }
+
+                if (x.ImplementationInstance is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, x.ImplementationInstance);
+                }
+
+
+                if (x.ImplementationFactory is not null)
+                {
+                    return new ServiceDescriptor(x.ServiceType, componentsKey, (provider, _) => x.ImplementationFactory(provider), x.Lifetime);
+                }
+            }
+
+            throw new NotImplementedException($"Cannot create new service descriptor for {x.Lifetime} {x.ServiceType}");
+        }));
     }
 
     private static Func<IEnumerable<T>, object>? BuildConverter<T>(Type to)
